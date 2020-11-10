8858b40f11514e2bfb8781fcf6dea9671b3211b9
tabpicker <51387405+tabpicker@users.noreply.github.com>
2020-11-10 11:44:04 -0800

    outer loop working

--- js/script.js
+++ js/script.js
@@ -16,17 +16,24 @@ showPage(data, 1); //calling the showPage function with the data object array an
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage(list,page){
+function showPage(list,page) {
    // create two variables which will represent the index for the first and last student on the page
    const startIndex = (page * 9) - 9;
    const endIndex= (page * 9) * 9;
 
+
+   console.log('Start index is');
+   console.log(startIndex);
+   console.log('endindex is');
+   console.log(endIndex);
+   console.log('list.length');
+   console.log(list.length)
    // select the element with a class of `student-list` and assign it to a variable
    const studentList = document.querySelector('.student-list');
    // set the innerHTML property of the variable you just created to an empty string
    studentList.innerHTML='';
-   // loop over the length of the `list` parameter
-   //for( var i = 0; i < list.length; i++ ) {
+   //loop over the length of the `list` parameter
+   for( var i = 0; i < list.length; i++ ) {
       // inside the loop create a conditional to display the proper students
       //for(var j = 0; startIndex < list.length < endIndex; j++) { 
       // inside the conditional:
@@ -42,16 +49,12 @@ function showPage(list,page){
            <span class="date">Joined 12-15-2005</span>
          </div>
        </li>`;
-         // const li = document.createElement(li); 
-         // const div = document.createElement(div);
-         // const h3 = document.createElement(h3);
-         // const img = document.createElement(img);
-         // const span = document.createElement(span);
 
          // insert the above elements
          studentList.insertAdjacentHTML('beforeend',studentItem);
       }
-   
+   //}
+}
 
 showPage(data, 1); //calling the showPage function with the data object array and page 1
 
